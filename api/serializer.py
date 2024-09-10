4e2c7301683a6b857c3393399a2adea4a36cc597
Juantrevi <juantrevi70@gmail.com>
2024-09-10 23:45:19 +0100

    -- Serializing for registering users --

--- api/serializer.py
+++ api/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, AuthUser
 from rest_framework_simplejwt.tokens import Token
+from django.contrib.auth.password_validation import validate_password
 
 from userauths.models import User, Profile
 
@@ -22,6 +23,34 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
 
         return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password', 'password2']
+
+    def validate(self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "Password fields do not match"})
+
+        return attr
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name = validated_data['full_name'],
+            email = validated_data['email'],
+        )
+
+        email_username, _ = user.email.split('@')
+        user.username = email_username
+        user.set_password(validated_data['password1'])
+        user.save()
+
+        return user
 
 
 # In this example, UserSerializer and
--- api/views.py
+++ api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from api import serializer as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+
+from userauths.models import User, Profile
+from rest_framework.permissions import AllowAny
 
 
 # Define a custom view for obtaining JWT tokens
 class MyTokenObtainPairView(TokenObtainPairView):
     # Specify the custom serializer to use for this view
     serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
