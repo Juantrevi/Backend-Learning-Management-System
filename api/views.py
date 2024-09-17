93d362267853e9375162f46f52cec6935137fe46
Juantrevi <juantrevi70@gmail.com>
2024-09-17 14:38:37 +0100

    -- Paypal configuration --

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 import random
+
+import requests
 from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import redirect
 from django.template.loader import render_to_string
@@ -23,6 +25,8 @@ env = Env()
 env.read_env()
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+PAYPAL_CLIENT_ID = settings.PAYPAL_CLIENT_ID
+PAYPAL_SECRET_ID = settings.PAYPAL_SECRET_ID
 
 """
 List of the generic views provided by Django REST Framework and the HTTP methods they correspond to:
@@ -552,9 +556,79 @@ class StripeCheckoutAPIView(generics.CreateAPIView):
             )
 
             order.stripe_session_id = checkout_session.id
-            order.save()
+            # order.save()
 
-            # return redirect(checkout_session.url)
-            return Response({'message': checkout_session.url})
+            return redirect(checkout_session.url)
+            # return Response({'message': checkout_session.url})
         except stripe.error.StripeError as e:
             return Response({'message': f'Something went wrong with the payment. Error: {str(e)}'})
+
+
+def get_access_token(client_id, secret_key):
+    """Function to get the paypal access
+    token from the frontend using client_id
+    and secret_key"""
+
+    token_url = "https://api.sandbox.paypal.com/v1/oauth/token"
+    data = {'grant_type': 'client_credentials'}
+    auth = (client_id, secret_key)
+    response = requests.post(token_url, data=data, auth=auth)
+
+    if response.status_code == 200:
+        print('Access Token === ', response.json()['access_token'])
+        return response.json()['access_token']
+    else:
+        raise Exception(f'Failed to get access token from paypal {response.status_code}')
+
+
+class PaymentSuccessAPIView(generics.CreateAPIView):
+    serializer_class = api_serializer.CartOrderSerializer
+    queryset = api_models.CartOrder.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        order_oid = request.data['order_oid']
+        session_id = request.data['session_id']
+        paypal_order_id = request.data['paypal_order_id']
+
+        order = api_models.CartOrder.objects.get(oid=order_oid)
+        order_items = api_models.CartOrderItem.objects.filter(order=order)
+
+        # Paypal payment success
+        if paypal_order_id != 'null':
+            paypal_api_url = f'https://api-m.sandbox.paypal.com/v2/checkout/orders/{paypal_order_id}'
+            headers = {
+                'Content-type': 'application/json',
+                'Authorization': f'Bearer {get_access_token(PAYPAL_CLIENT_ID, PAYPAL_SECRET_ID)}'
+            }
+            response = requests.get(paypal_api_url, headers=headers)
+            if response.status_code == 200:
+                paypal_order_data = response.json()
+                paypal_payment_status = paypal_order_data['status']
+                if paypal_payment_status == 'COMPLETED':
+                    if order.payment_status == 'Processing':
+                        order.payment_status = "Paid"
+                        order.save()
+                        return Response({'message': 'Payment Successful'})
+
+                    else:
+                        return Response({'message': 'Already Paid'})
+                else:
+                    return Response({'message': 'Payment Failed'})
+            else:
+                return Response({'message': 'PayPal Error Occurred'})
+
+        # Stripe Payment success
+        if session_id != 'null':
+            session = stripe.checkout.Session.retrieve(session_id)
+            if session.payment_status == 'paid':
+                if order.payment_status == 'Processing':
+                    order.payment_status = "Paid"
+                    order.save()
+                    return Response({'message': 'Payment Successful'})
+                else:
+                    return Response({'message': 'Already Paid'})
+            else:
+                return Response({'message': 'Payment Failed'})
+
+
+
